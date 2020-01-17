9485b1aea20d3a55e8eb6f2ce6e332a4fd23bf99
ivannarodriguez <imr6@students.calvin.edu>
2020-01-17 18:19:38 -0500

    played around with layout for dropdowns and chart elements

--- interactive.js
+++ interactive.js
@@ -1,9 +1,13 @@
 // Margin Convention
 let margins = { top: 20, right: 25, bottom: 30, left: 40 }
-let svgouterWidth = 450
-let svgouterHeight = 450
-let innerWidth = 450 - margins.left - margins.right
-let innerHeight = 450 - margins.top - margins.bottom
+let svgouterWidth = 800
+let svgouterHeight = 350
+let innerWidth = svgouterWidth - margins.left - margins.right
+let innerHeight = svgouterHeight - margins. top - margins.bottom
+
+d3.select('div#dropdowns')
+    .attr('width', svgouterWidth)
+    .style('backgrpund-color', "gray");
 
 //append svg to the body of the page
 let svg = d3.select('div#canvas svg#chart')
@@ -11,5 +15,4 @@ let svg = d3.select('div#canvas svg#chart')
             .attr('height', svgouterHeight)
             .append('g')
               .attr('id', "plot-area")
-              .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'
-              .append('label');
+              .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
