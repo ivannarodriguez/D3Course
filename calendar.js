cbf0c922cd0f790ef287b71fdc2a4a945ecc2be3
iel3 <iel3@maroon24.cs.calvin.edu>
2020-01-20 10:00:51 -0500

    trying to get the gracePeriodEnd function to work

--- calendar.js
+++ calendar.js
@@ -1,11 +1,23 @@
 const graceDays = 60
 
-let graduationDate = d3.select('#graduation').property('value')
+let graduationDate = d3.select('#graduation')
 console.log(graduationDate)
 let startingdate = d3.select('#startingdate').property('value')
 console.log(startingdate)
 console.log( typeof graduationDate)
 
 function gracePeriodEnd (str){
-    //d3.select('#startingdate').setAttribute('max',)
+    var date = new Date(str);
+    date.setDate(date + graceDays);
+    return date
 }
+  
+
+
+console.log(gracePeriodEnd(graduationDate))
+var date = new Date(graduationDate);
+console.log(date)
+console.log(typeof date)
+console.log( typeof graduationDate)
+var newDate = date.setDate(date + gracePeriodEnd)
+console.log(newDate)
