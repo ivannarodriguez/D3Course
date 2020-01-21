fc62dc809a9b56acd81fbd62b2a2c3a30ab850fc
ss63 <ss63@maroon13.cs.calvin.edu>
2020-01-21 11:23:00 -0500

    pushing stem

--- stem.js
+++ stem.js
@@ -4,19 +4,21 @@ const stemOptDuration = 2 //2 years
 // store variable
 let qualifyStem = d3.select(#stemExt).property.('value')
 
-function endDate (str) {
-  var date = new Date(str)
-  var endDateTemp = new Date(date.setFullYear(date.getFullYear() + optDuration))
-  var endDate = endDateTemp.toJSON().slice(0,10)
-  return endDate
+// Function takes in OPT start date and calculates OPT end date
+let endDate = function endDate (str) {
+    var date = new Date(str)
+    var endDateTemp = new Date(date.setFullYear(date.getFullYear() + optDuration))
+    var endDate = endDateTemp.toJSON().slice(0,10)
+    return endDate
 }
 
-function stemStartDate (str) {
+// Function takes regular OPT end date and adds 2 years calculate
+function stemEndDate (str) {
   if qualifyStem === 'Yes' {
     var date = new Date(str)
-    var stemStartDateTemp = new Date(date.setDate(date.getDate() + stemOptDuration))
-    var stemStartDate = stemStartDate.toJSON().slice(0,10)
-    return stemStartDate
+    var stemEndDateTemp = new Date(date.setFullYear(date.setFullYear() + stemOptDuration))
+    var stemEndDate = stemEndDate.toJSON().slice(0,10)
+    return stemEndDate
   }
 
 }
