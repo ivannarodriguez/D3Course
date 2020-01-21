ad59e01704fc6f97c8d8f8ee72f5ab6f1867da34
iel3 <iel3@maroon24.cs.calvin.edu>
2020-01-21 09:40:18 -0500

    Incorporated functions for calculating dates and setting dates on the calendar

--- opt.js
+++ opt.js
@@ -1,3 +1,59 @@
+const graceDays = 60 //60 days
+const earliestApplication = -90 //-90 days
+const optDuration = 1 // 1 year
+
+
+let graduationDate = d3.select('#graduation').property('value')
+
+let startingdate = d3.select('#startingdate').property('value')
+
+
+// Function that recieves graduation date and calculates the date of the 60 day grace 
+//period after graduation
+//Recieves string in format year-month-day and returns string in same format
+function gracePeriodEnd (str){
+    var date = new Date(str);
+    var newDate = new Date(date.setDate(date.getDate() + graceDays));
+    var finalNewDate = newDate.toJSON().slice(0,10);
+    return finalNewDate;
+}
+
+
+// Function that recieves graduation date and calculates the date of the earliest 
+// possible application day 
+//Recieves string in format year-month-day and returns string in same format
+function earlyApplicationDate (str){
+    var date = new Date (str);
+    var newDate = new Date(date.setDate(date.getDate()+ earliestApplication));
+    var finalNewDate = newDate.toJSON().slice(0,10);
+    return finalNewDate;
+}
+
+
+// Function that recieves starting date and calculates the end date of OPT
+////Recieves string in format year-month-day and returns string in same format
+function endOPT (str){
+    var date = new Date(str);
+    var newDate = new Date(date.setFullYear(date.getFullYear()+ optDuration))
+    var finalNewDate = newDate.toJSON().slice(0,10);
+    return finalNewDate;
+}
+
+//Function that recieves string with grace period end date and sets the max date on the
+// starting date calendar
+function setGracePeriodEnd (str){
+    d3.select('#startingdate')
+    .property('max', str);  
+}
+
+//Function that recieves string with graduation date and set min date on the starting date
+//calendar
+function setGracePeriodStart (str){
+    d3.select('#startingdate')
+    .property('min', str)
+}
+
+
 // Margin Convention
 let margins = { top: 20, right: 25, bottom: 30, left: 40 }
 let svgouterWidth = 800
@@ -74,7 +130,7 @@ let svg = d3.select('div#canvas svg#chart')
           .attr('y', innerHeight/2)
           .attr('width', tlength/3)
           .attr('height', r2);
-        svg.append('rect')
+        svg.append('rect')                      
           .attr('class', 'dottedline')
           .attr('x', 0)
           .attr('y', innerHeight/4)
@@ -150,8 +206,6 @@ let svg = d3.select('div#canvas svg#chart')
           .attr("cy", innerHeight/2 + r2)
           .attr("r", r1);
 
-
-        
 
 
 
