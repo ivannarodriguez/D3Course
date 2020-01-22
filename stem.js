d04500ec70c3422b430f55a1ad6440b109aa18b2
ss63 <ss63@maroon13.cs.calvin.edu>
2020-01-22 10:38:24 -0500

    fixed errors in stemEndDate function -- I've tested to ensure that it works

--- stem.js
+++ stem.js
@@ -1,8 +1,8 @@
 const optDuration = 1 //2 years
 const stemOptDuration = 2 //2 years
 
-// store variable
-let qualifyStem = d3.select(#stemExt).property.('value')
+
+let qualifyStem = d3.select('#stemExt').property('value')
 
 // Function takes in OPT start date and calculates OPT end date
 let endDate = function endDate (str) {
@@ -11,20 +11,16 @@ let endDate = function endDate (str) {
     var endDate = endDateTemp.toJSON().slice(0,10)
     return endDate
 }
+console.log(endDate('2019-01-28'))
 
 // Function takes regular OPT end date and adds 2 years calculate
 function stemEndDate (str) {
-  if qualifyStem === 'Yes' {
+  if (qualifyStem === 'Yes') {
     var date = new Date(str)
-    var stemEndDateTemp = new Date(date.setFullYear(date.setFullYear() + stemOptDuration))
-    var stemEndDate = stemEndDate.toJSON().slice(0,10)
+    var stemEndDateTemp = new Date(date.setFullYear(date.getFullYear() + stemOptDuration))
+    var stemEndDate = stemEndDateTemp.toJSON().slice(0,10)
     return stemEndDate
   }
-
 }
 
-function stemDrawLine (str) [
-  if qualifyStem === 'Yes' {
-    // ivanna's code
-  }
-]
+console.log(stemEndDate('2020-01-29'))
