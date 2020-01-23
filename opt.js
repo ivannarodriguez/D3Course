1d6a52bf44c77f532bc63d8020a4a7ea1b668c61
imr6 <imr6@maroon12.cs.calvin.edu>
2020-01-23 12:13:01 -0500

    commit

--- opt.js
+++ opt.js
@@ -90,226 +90,56 @@ d3.select('#graduation').on('input', setGracePeriodEnd)
 * S T A R T S  H E R E
 */
 
-// Margin Convention
-let margins = { top: 20, right: 25, bottom: 30, left: 40 }
-let svgouterWidth = 800
-let svgouterHeight = 500
-let innerWidth = svgouterWidth - margins.left - margins.right
-let innerHeight = svgouterHeight - margins.top - margins.bottom
-let tlength = innerWidth/2.6
-let r1 = 15
-let r2 = 45
+let circledata = [{ x: 0, y: 300, r: 15, group: 'endpoint', html: ''}, //-90
+                { x: 300, y: 300, r: 30, group: 'grad', html: ''},      //grad
+                { x: 500, y: 300, r: 15, group: 'endpoint', html: ''},  //60
+                { x: 1300, y: 600, r: 15, group: 'endpoint', html: ''}, //optend
+                { x: 1400, y: 600, r: 15, group: 'endpoint', html: ''}, //opt60
+                { x: 1000, y: 900, r: 15, group: 'endpoint', html: ''}, //stemstartapplying
+                { x: 1300, y: 900, r: 15, group: 'endpoint', html: ''},] //stemstart
 
-d3.select('div#dropdowns')
-    .attr('width', svgouterWidth);
-//append svg to the body of the page
-let svg = d3.select('div#canvas svg#chart')
-            .attr('width', svgouterWidth)
-            .attr('height', svgouterHeight)
-            .append('g')
-              .attr('id', "plot-area")
-              .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
-    // USCIS can receive your app section
-    // horizontal line
-    svg.append("line")
-      .attr("class", "blackline")
-      .attr('x1',0)
-      .attr('y1',innerHeight/2)
-      .attr('x2',tlength)
-      .attr('y2',innerHeight/2);
-    //-90 days (leftmost circle and line)
-    svg.append("circle")
-      .attr("id","ninetydays")
-      .attr("class", "endpoints")
-      .attr("cx", 0)
-      .attr("cy", innerHeight/2)
-      .attr("r", r1);
-    svg.append("line")
-      .attr("class", "blackline")
-      .attr('x1',0)
-      .attr('y1',innerHeight/2-r1)
-      .attr('x2',0)
-      .attr('y2',innerHeight/2+r1);
-    //+60 days  (rightmost circle and line)
-    svg.append("circle")
-      .attr("class", "endpoints")
-      .attr("cx", tlength) //x posiion plus radius
-      .attr("cy", innerHeight/2)
-      .attr("r", r1);
-    svg.append("line")
-        .attr("class", "blackline")
-        .attr('x1',tlength)
-        .attr('y1',innerHeight/2-r1)
-        .attr('x2',tlength)
-        .attr('y2',innerHeight/2+r1);
-    
-     //Graduation label
-    svg.append("circle")
-     .attr("id", "gradcircle")
-     .attr("cx", tlength -  tlength/3)
-     .attr("cy", innerHeight/2)
-     .attr("r", 30)
-     .attr("fill", "#97252B")
-     .attr("opacity", 0.4);
-   svg.append("text")
-     .text('GRAD')
-     .attr('id', 'gradlabel')
-     .style('fill', 'white')
-     .attr('x', tlength -  tlength/3 - 1.2*r1)
-     .attr('y', innerHeight/2 -r1/2);
 
-    //dotted lines
-    //bottom
-    svg.append('rect')
-      .attr('class', 'dottedline')
-      .attr('x', tlength -  tlength/3 )
-      .attr('y', innerHeight/2)
-      .attr('width', tlength/3)
-      .attr('height', 2 * r2);
-    //top
-    svg.append('rect')
-      .attr('class', 'dottedline')
-      .attr('x', 0)
-      .attr('y', innerHeight/4)
-      .attr('width', tlength)
-      .attr('height', innerHeight/4);
+//adding a margin to the svg
+let margin = {top: 5, bottom: 5, left: 50, right: 20 };
+let svgWidth = 1000;
+let svgHeight = 1000;
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeight - margin.top - margin.bottom;
 
-    //text
-    // svg.append("text")
-    //   .text('-90 days')
-    //   .attr('class', 'labels')
-    //   .attr('x', tlength/8 + r1)  
-    //   .attr('y', innerHeight/2 - r1/2);
-    svg.append("text")
-      .text('USCIS can receive your application')
-      .attr('class', 'labels')
-      .attr('x', tlength/8)
-      .attr('y', innerHeight/4.5);
-    svg.append("text")
-      .text('OPT Starts')
-      .attr('class', 'labels')
-      .attr('x', tlength-1.80*r2)
-      .attr('y', innerHeight/2 + 2.8 * r2);
-  //  svg.append("text")
-  //     .text('+60 days')
-  //     .attr('class', 'labels')
-  //     .attr('x', tlength/8 + 4 * r2)  
-  //     .attr('y', innerHeight/2 - r1/2);
-    svg.append("text")
-      .text('OPT Ends')
-      .attr('class', 'labels')
-      .attr('x', 2 * tlength)
-      .attr('y', innerHeight/2 + 2.8 * r2);
-    svg.append("text")
-      .text('60 days')
-      .attr('class', 'labels')
-      .attr('x', 2.17 * tlength)
-      .attr('y', innerHeight/2 + 1.75 * r2);
+// create svg for scatterplot to b at
+let canvas = d3.select("body")
+                .append("svg")
+                .attr("width", svgWidth)
+                .attr("height", svgHeight)
+                // .style('background-color', 'yellow')
+                .append("g")
+                  .attr('transform', "translate(" + margin.left + "," + margin.top + ")");
 
-    // One year OPT timeline
-    // vertical lines and gray rectangle
-    svg.append('rect')
-      .attr('id', 'tp')
-      .attr('class', 'endpoints')
-      .style('opacity', '0.3')
-      .attr('x', tlength -  tlength/3)
-      .attr('y', innerHeight/2 + 1.85 * r2)
-      .attr('width', tlength/3)
-      .attr('height', r1);
-    svg.append("line")
-      .attr("class", "blackline")
-      .attr('x1',tlength -  tlength/3)
-      .attr('y1', innerHeight/2 + 2 * r2 - r1)
-      .attr('x2',tlength -  tlength/3)
-      .attr('y2', innerHeight/2 + 2 * r2 + r1);
-    svg.append("line")
-      .attr("class", "blackline")
-      .attr('x1',tlength)
-      .attr('y1', innerHeight/2 + 2 * r2 - r1)
-      .attr('x2',tlength)
-      .attr('y2', innerHeight/2 + 2 * r2 + r1);
-    // long line
-    svg.append("line")
-      .attr("class", "blackline")
-      .attr("id", "optline")
-      .attr('x1', tlength)
-      .attr('y1', innerHeight/2 + 2 * r2)
-      .attr('x2', 2 * tlength + 2 * r1)
-      .attr('y2', innerHeight/2 + 2 * r2);
-    // right lil line
-    svg.append("line")
-      .attr("class", "blackline")
-      .attr('x1', 2 * tlength + 2 * r1)
-      .attr('y1', innerHeight/2 + 2 * r2 - r1)
-      .attr('x2', 2 * tlength + 2 * r1)
-      .attr('y2', innerHeight/2 + 2 * r2 + r1);
-    svg.append("circle")
-      .attr("class", "endpoints")
-      .attr("cx", 2 * tlength + 2 * r1) //x posiion plus radius
-      .attr("cy", innerHeight/2 + 2 * r2)
-      .attr("r", r1);
-    
-    // 60 days after OPT
-    // dotted 60 days line
-    svg.append("line")
-      .attr("class", "dottedline")
-      .attr('x1', 2.1 * tlength)
-      .attr('y1', innerHeight/2 + 2 * r2)
-      .attr('x2', 2.1 * tlength + 2 * r2) // 2.21 line length
-      .attr('y2', innerHeight/2 + 2 * r2);
-    // right most lil line
-    svg.append("line")
-      .attr("class", "blackline")
-      .attr('x1', 2.1 * tlength + 2 * r2)
-      .attr('y1', innerHeight/2 + 2 * r2 - r1)
-      .attr('x2', 2.1 * tlength + 2 * r2) // 2.21 line length
-      .attr('y2', innerHeight/2 + 2 * r2 + r1);
-    svg.append("circle")
-      .attr("class", "endpoints")
-      .attr("cx", 2.1 * tlength + 2 * r2) //x posiion plus radius
-      .attr("cy", innerHeight/2 + 2 * r2)
-      .attr("r", r1);
-    
-  
-    /* Way too many tooltips:
-     there SHOULD be an easier way...we need to
-     update the tooltip text depending on the id of 
-     the svg element selected...how do we do this???
-    */
-          
-    let tooltip =  d3.select('div#canvas')
-      .append('div')
-      .attr("class", "tooltip")
-      .style("position", "absolute")
-      .style("visibility", 'hidden');
-    
-    //start application tooltip
-    d3.select('g#plot-area')
-      .select('circle#ninetydays')
-      .on('mouseover', function(){return tooltip.style("visibility", "visible");})
-      .on('mousemove', function(){ return d3.select('.tooltip')
-          .html("Take about a week before this date "+
-          "to collect your documents so you can "+
-          "mail them to USCIS <strong>on this date.</strong> "+
-          "Prime time for USCIS to receive your application "+
-          "is between now and graduation.");})
-      .on("mouseleave", function(){return tooltip.style("visibility", "hidden");});
+//horizontal scale
+let xScale = d3.scaleLinear()
+  .domain(d3.extent(circledata.map(d => d.x)))
+  .range([10,600])
 
-    //OPT Start Tooltip
-    d3.select('g#plot-area')
-    .selectAll('rect#tp')
-    .on('mouseover', function(){return tooltip.style("visibility", "visible");})
-    .on('mousemove', function(){ return d3.select('.tooltip')
-        .html("You can request any start date that is within 60 days from your program end date (graduation). You can only start working once you have your EAD card in hand, and on or after the start date your EAD card specifies.");})
-    .on("mouseleave", function(){return tooltip.style("visibility", "hidden");});
-    
-    //during opt tooltip
-    d3.select('g#plot-area')
-      .selectAll('line#optline')
-      .on('mouseover', function(){return tooltip.style("visibility", "visible");})
-      .on('mousemove', function(){ return d3.select('.tooltip')
-          .html("During this time, you can: </br> 1. Work <br/> 2. Transfer SEVIS record for grad school");})
-      .on("mouseleave", function(){return tooltip.style("visibility", "hidden");});
+//vertical scale
+let yScale = d3.scaleLinear()
+  .domain(d3.extent(circledata.map(d => d.y)))
+  .range([10, 250]); //backwards because 0,0 is at top left corner
+
+
+let circles = canvas.selectAll('circle')
+                    .data(circledata)
+                      .enter()
+                      .append('circle')
+                        .attr('cx', d => xScale(d.x))
+                        .attr('cy', d=> yScale(d.y))
+                        .attr('r', d=> d.r)
+                        .attr('class', d=> d.group)
+                        .style('fill', 'black');
+
+
+
+
+
 
 
 
